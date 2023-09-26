b8232aa4e385d9fbb4974ac1e62862c845184fd7
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-09-26 22:53:07 +0530

    Update Leet_Contains_Duplicate.java

--- Leet_Contains_Duplicate.java
+++ Leet_Contains_Duplicate.java
@@ -5,7 +5,7 @@ class Solution {
 
         for (int i = 0; i < nums.length - 1; i++)
 
-            if (nums[i] == nums[i++])
+            if (nums[i] == nums[i + 1])
 
                 return true;
 
