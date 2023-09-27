a68c88b13939282574e49af50ec37ef30e849ccc
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-09-27 15:50:18 +0530

    Update Leet_Find_the_difference.java

--- Leet_Find_the_difference.java
+++ Leet_Find_the_difference.java
@@ -1,17 +1,17 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         
-        char[] S = s.toCharArray();
-        char[] T = t.toCharArray();
+        char[] s2 = s.toCharArray();
+        char[] t2 = t.toCharArray();
 
         Arrays.sort(S);
         Arrays.sort(T);
 
-        // int l1 = S.length;
-        // int l2 = T.length;
+        // int l1 = s2.length;
+        // int l2 = t2.length;
         int i=0;
 
-        while(i < l1 && i <= l2){
+        while(i < s2.length && i <= t2.length){
 
             if(S[i] != T[i]){
 
