05f4e7aa669d0c3575d4e759d97e3ab427a71ba4
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-09-28 20:09:39 +0530

    Update 1920. Build Array from Permutation.java

--- 1920. Build Array from Permutation.java
+++ 1920. Build Array from Permutation.java
@@ -3,9 +3,11 @@ class Solution {
         
         int[] ans = new int[nums.length];
 
-        for (int i = 0; i <= nums.length; i++)
+        for (int i = 0; i < nums.length; i++)
             ans[i] = nums[nums[i]];
 
         return ans;
     }
 }
+
+// need to improve on memory.........
