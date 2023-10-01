9f23811fc180ce0ca111d674593992cf549d6544
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-10-01 21:00:23 +0530

    Create 1672 Richest Customer Wealth.java

---
+++ 1672 Richest Customer Wealth.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+        int large = accounts[0][0];
+
+        for(int customer[] : accounts){
+
+            int Sum = 0;
+            
+            for(int bank : customer)
+                Sum += bank;
+
+            large = Math.max(large, Sum);
+        }
+        return large;
+    }
+}
