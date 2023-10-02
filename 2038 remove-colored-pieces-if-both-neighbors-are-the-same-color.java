e955e8f70f93beeeef70fd0f942dee5d272acc9f
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-10-02 20:58:35 +0530

    Update 2038 remove-colored-pieces-if-both-neighbors-are-the-same-color.java

--- 2038 remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038 remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,1 +1,31 @@
+class Solution {
+    public boolean winnerOfGame(String colors) 
+    {
+        int a = 0, b = 0;
 
+        if (colors.length() < 3)
+            return false;
+
+        for (int i = 1; i < colors.length() - 1;i++)
+        {
+          if (colors.charAt(i) == 'A')
+          {
+          if (colors.charAt(i - 1)  == 'A' && colors.charAt(i + 1)  == 'A')
+            a++;
+          }
+
+          else if (colors.charAt(i) == 'B')
+          {
+          if (colors.charAt(i - 1)  == 'B' && colors.charAt(i + 1)  == 'B')
+            b++;
+          }
+        }
+
+        if (b >= a )
+          return false;
+
+        else
+          return true;
+        
+    }
+}
