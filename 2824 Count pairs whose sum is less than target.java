512685ab25c55986e9d26889f7d58e787eb91d81
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-10-02 19:21:08 +0530

    Update 2824 Count pairs whose sum is less than target.java

--- 2824 Count pairs whose sum is less than target.java
+++ 2824 Count pairs whose sum is less than target.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        
+        int count = 0;
 
+        for (int i = 0; i < nums.size() - 1; i++) 
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums.get(j) + nums.get(i) < target)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
