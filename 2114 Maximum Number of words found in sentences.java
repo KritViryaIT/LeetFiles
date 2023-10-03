470ff59cf81385e0d0193c041d6a2cf232f6527d
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-10-03 21:00:47 +0530

    Update 2114 Maximum Number of words found in sentences.java

--- 2114 Maximum Number of words found in sentences.java
+++ 2114 Maximum Number of words found in sentences.java
@@ -1,1 +1,31 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        
+        int max = 0;
 
+        for (String s : sentences)
+        {
+            char ch[]= new char[s.length()];
+            int count = 0;
+
+            for(int i=0; i < s.length(); i++)  
+            {  
+                ch[i]= s.charAt(i);
+
+                if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) )  
+                    count++;  
+            }
+
+            if (count > max)
+                max = count;  
+        }
+
+        return max;
+    }
+}
+
+/*          Shorter Way but not optimal too much time and space complexity
+
+
+
+*/
