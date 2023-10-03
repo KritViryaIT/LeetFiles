a231bb35afda98e0f322b03ac01810783639d898
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-10-03 21:44:49 +0530

    Update 2574 Left and right Sum Differences.java

--- 2574 Left and right Sum Differences.java
+++ 2574 Left and right Sum Differences.java
@@ -38,6 +38,25 @@ class Solution {
 
 */
 
-// optimal solution
+//                         optimal solution
+
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        
+        int[] l = new int[nums.length];
+        int[] r = new int[nums.length];
+
+        for ( int i = 1; i < nums.length; i++)
+            l[i] = l[i-1] + nums[i - 1];
+
+        for (int i = nums.length - 2; i >=0; i--)
+            r[i] = r[i + 1] + nums[i + 1];
+
+        for (int i = 0; i < nums.length; i++)
+            nums[i] = Math.abs(l[i] - r[i]);
+
+        return nums;
+    }
+}
 
 
