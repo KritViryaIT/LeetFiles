0f019e53efa8893858fa0c635e61c004041881d7
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-10-03 21:33:53 +0530

    Create 2574 Left and right Sum Differences.java

---
+++ 2574 Left and right Sum Differences.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            int l = 0, r = 0;
+            
+            for (int j = 0; j < nums.length; j++)
+            {
+                if (j > i)
+                    r += nums[j];
+
+                else if (j < i)
+                    l += nums[j];
+
+                else if (i == 0)
+                    l = 0;
+
+                else if (i == nums.length - 1)
+                    r = 0;
+            }
+
+            ans[i] = l - r;
+
+            if (ans[i] < 0)
+                ans[i] = 0 - ans[i];
+
+        }
+
+        return ans;
+    }
+}
