e7f3ab6ffea39f181f30a5e8b2febe807abec7f5
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-10-04 12:39:16 +0530

    Update Shuffle String.java

--- Shuffle String.java
+++ Shuffle String.java
@@ -1,1 +1,13 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        
+        StringBuilder sb = new StringBuilder("");
+        char  ch[]  = new char[s.length()];
 
+        for (int i = 0; i < s.length(); i++)
+            ch[indices[i]] = s.charAt(i);
+        
+        sb.append(ch);
+        return sb.toString();
+    }
+}
