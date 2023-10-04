17eab7b91e50fa9247b122a98459efe1a299af40
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-04 13:30:34 +0530

    Update 1816 Truncate Sentences.java

--- 1816 Truncate Sentences.java
+++ 1816 Truncate Sentences.java
@@ -30,15 +30,11 @@ class Solution {
         
         for (int i = 0; i < s.length(); i++)
         {   
-            if (s.charAt(i) == ' ')
-                count++;
+            if (s.charAt(i) == ' ') count++;
 
-            if (count == k)
-                return s.substring(0,i);
+            if (count == k) return s.substring(0,i);
             
         }
-
         return s;
-        
     }
 }
