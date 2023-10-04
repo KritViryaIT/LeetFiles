7e3018d11bf379857010e96c7398e997bd705e6c
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-10-04 12:42:05 +0530

    Update Shuffle String.java

--- Shuffle String.java
+++ Shuffle String.java
@@ -11,3 +11,17 @@ class Solution {
         return sb.toString();
     }
 }
+
+// --------------- Another Method --------------
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        
+        char  ch[]  = new char[s.length()];
+
+        for (int i = 0; i < s.length(); i++)
+            ch[indices[i]] = s.charAt(i);
+        
+        return String.valueOf(ch);
+    }
+}
