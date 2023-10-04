71dadc9709124606e88e8d95734e4ec0da493b38
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-04 13:24:56 +0530

    Create 1816 Truncate Sentences.java

---
+++ 1816 Truncate Sentences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        
+        int count = 0;
+        String s2 = "";
+        for (int i = 0; i < s.length(); i++)
+        {   
+            if (s.charAt(i) == ' ')
+                count++;
+
+            if (count == k)
+                break;
+
+            s2 += s.charAt(i);
+            
+        }
+
+        return s2;
+    }
+}
+
+// Above is one of the most basic approach.....
+
+// Optimised
