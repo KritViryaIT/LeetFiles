1a38bc79c45b08e81ca4d9ed24439f353cb019f8
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-04 13:04:48 +0530

    Update 1662 Check if two strings are equivalent.java

--- 1662 Check if two strings are equivalent.java
+++ 1662 Check if two strings are equivalent.java
@@ -14,3 +14,18 @@ class Solution {
 }
 
 // memory hungry
+
+// ---------------------- Another Better Approach ---------------------- 
+
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        
+        for (int i = 1; i < word1.length; i++)
+            word1[0] += word1[i];
+
+        for (int i = 1; i < word2.length; i++)
+            word2[0] += word2[i];
+
+        return word1[0].equals(word2[0]);
+    }
+}
