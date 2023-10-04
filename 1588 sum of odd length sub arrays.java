8a885eb7b4422dd7d18e11189b89067db87bf635
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-04 20:58:56 +0530

    Update 1588 sum of odd length sub arrays.java

--- 1588 sum of odd length sub arrays.java
+++ 1588 sum of odd length sub arrays.java
@@ -4,6 +4,7 @@ class Solution {
         int sum = 0;
         for (int i = 0; i < arr.length; i++)
             sum += (arr[i] * ((((i + 1) * (arr.length - i)) + 1) / 2));
+        
         return sum;
     }
 }
