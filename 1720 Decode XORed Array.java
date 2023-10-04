3d056bd720b585d7930ca39a8517df88d9fb2724
KritViryaIT <111434417+KritViryaIT@users.noreply.github.com>
2023-10-04 12:22:52 +0530

    Update 1720 Decode XORed Array.java

--- 1720 Decode XORed Array.java
+++ 1720 Decode XORed Array.java
@@ -5,7 +5,7 @@ class Solution {
         int[] dec = new int[n + 1];
         dec[0] = first;
         for(int i = 1; i <= n; i++){
-            dec[i] = (encoded[i - 1] ^ dec[i - 1]); 
+            dec[i] = (encoded[i - 1] ^ dec[i - 1]);     // ' ^ ' This is used to do bitwise exclusive OR (XOR) operations
         }
         return dec;
     }
