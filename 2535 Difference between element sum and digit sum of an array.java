9c67d78c7e9856d7897e29ad6e79aa151d30b796
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-05 22:25:29 +0530

    Update 2535 Difference between element sum and digit sum of an array.java

--- 2535 Difference between element sum and digit sum of an array.java
+++ 2535 Difference between element sum and digit sum of an array.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        
+        int d = 0, e = 0;
 
+        for (int i : nums)
+        {
+
+            e += i;
+            while (i > 0)
+            {
+                d += i%10;
+                i /= 10;
+            }
+        }
+
+        return Math.abs(d - e);
+    }
+}
