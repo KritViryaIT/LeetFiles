9be174412798af7e497ad3f40b2aa7376ade39d7
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-07 18:09:29 +0530

    Update 1534 count-good-triplets.java

--- 1534 count-good-triplets.java
+++ 1534 count-good-triplets.java
@@ -1,3 +1,5 @@
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
 int count = 0;
         for(int i = 0; i < arr.length - 2; i++) {
             for(int j = i + 1; j < arr.length - 1; j++) {
