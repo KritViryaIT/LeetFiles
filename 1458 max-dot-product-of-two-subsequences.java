5c182cfbf9b4d3a043afb283b48f7a6032400996
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-08 18:15:17 +0530

    Update 1458 max-dot-product-of-two-subsequences.java

--- 1458 max-dot-product-of-two-subsequences.java
+++ 1458 max-dot-product-of-two-subsequences.java
@@ -6,13 +6,18 @@ class Solution {
         int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 0; i <= n; i++) {
+            
             for (int j = 0; j <= m; j++) {
+                
                 if (i == 0 || j == 0) {
                     dp[i][j] = Integer.MIN_VALUE;
-                } else {
+                } 
+                else {
                     dp[i][j] = Math.max(
+                        
                         nums1[i - 1] * nums2[j - 1] + Math.max(dp[i - 1][j - 1], 0),
                         Math.max(dp[i - 1][j], dp[i][j - 1])
+                        
                     );
                 }
             }
