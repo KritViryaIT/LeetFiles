cf824514cedc8e368f4d3116bbbadc2fe22f7418
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-10 13:38:13 +0530

    Update 034 find-first-and-last-position-of-element-in-sorted-array.java

--- 034 find-first-and-last-position-of-element-in-sorted-array.java
+++ 034 find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,3 +1,10 @@
-
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+       
+        int[] ans = {-1,-1};
+
+        if (nums.length == 1 && nums[0] == target)
+        {
+            int[] ans2 = {0,0};
+            return ans2;
+        }
