f7ecfe8d1d0722d6cfbd8fd457d8f7bd54c5e809
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-10 13:43:57 +0530

    Update 2009  minimum-number-of-operations-to-make-array-continuous.java

--- 2009  minimum-number-of-operations-to-make-array-continuous.java
+++ 2009  minimum-number-of-operations-to-make-array-continuous.java
@@ -1,1 +1,6 @@
-
+class Solution {
+    public int minOperations(int[] nums) {
+        Arrays.sort(nums);
+        int count=Integer.MAX_VALUE,j=1,dup=0;
+        int[] dupArr = new int[nums.length];
+        for(int i=0;i<nums.length;i++) {
