ff830260ff38a7fab3c5a7d018bb18f5ae4f7729
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-10 14:35:19 +0530

    Update 012 Integer to Roman.java

--- 012 Integer to Roman.java
+++ 012 Integer to Roman.java
@@ -1,1 +1,4 @@
-
+class Solution {
+    public String intToRoman(int num) {
+        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
