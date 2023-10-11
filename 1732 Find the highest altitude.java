9733c76bab1e7377f1e42bf30a14882beaf7a7bd
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-11 15:42:03 +0530

    Update 1732 Find the highest altitude.java

--- 1732 Find the highest altitude.java
+++ 1732 Find the highest altitude.java
@@ -1,1 +1,16 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
 
+        int[] alt = new int[gain.length+1];
+ 
+        for (int i=0; i < gain.length; i++)
+            alt[i+1] = alt[i]+gain[i];
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i=0; i < alt.length; i++)       
+            if (max < alt[i]) max = alt[i];
+
+        return max < 0 ? 0 : max;
+    }
+}
