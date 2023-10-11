363c22d913ffdacb40d7dd749dd7b994b475695c
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-11 15:32:16 +0530

    Update 2418 Sort the people.java

--- 2418 Sort the people.java
+++ 2418 Sort the people.java
@@ -3,3 +3,16 @@ class Solution {
         
         int l=heights.length;
         Map<Integer,String> map=new HashMap();
+
+        for(int i=0;i<l;i++)
+            map.put(heights[i],names[i]);
+        Arrays.sort(heights);
+
+        String[] ans=new String[l];
+
+        for(int i=l-1,j=0;i>=0;i--)
+            ans[j++]=map.get(heights[i]);
+
+    return ans;
+    }
+}
