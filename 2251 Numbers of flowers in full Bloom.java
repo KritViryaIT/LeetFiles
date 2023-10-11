80caed79cdca0e0aec58978b3962744cbacf58d9
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-11 14:50:48 +0530

    Update 2251 Numbers of flowers in full Bloom.java

--- 2251 Numbers of flowers in full Bloom.java
+++ 2251 Numbers of flowers in full Bloom.java
@@ -3,3 +3,15 @@ class Solution {
 
         int n = people.length;
         int[] result = new int[n];
+       
+        // Create a TreeMap to track flower intervals and their frequencies.
+
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for (int i = 0; i < flowers.length; i++) 
+        {
+            // Increment the start time frequency.
+            treeMap.put(flowers[i][0], treeMap.getOrDefault(flowers[i][0], 0) + 1);
+
+            // Decrement the end time frequency (add 1 to end time to indicate the end of the interval).
+            treeMap.put(flowers[i][1] + 1, treeMap.getOrDefault(flowers[i][1] + 1, 0) - 1);
+        }
