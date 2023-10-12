68ef091fdf2ea4d9288b9edba95061d20c5b1c64
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-12 13:50:21 +0530

    Update 1863 Sum of all subset XOR Totals.java

--- 1863 Sum of all subset XOR Totals.java
+++ 1863 Sum of all subset XOR Totals.java
@@ -1,12 +1,14 @@
 class Solution {
-    public int subsetXORSum(int[] nums) {
-        
+    public int subsetXORSum(int[] nums) 
+    {
         return getAns(nums,0,0);
     }
    
-    int getAns(int[] arr,int i,int cur){
-
+    int getAns(int[] arr,int i,int cur
+    {
         if(i==arr.length) return cur;
         return getAns(arr,i+1,cur^arr[i]) + getAns(arr,i+1,cur);
     }
 }
+
+// Need to improve on memory....
