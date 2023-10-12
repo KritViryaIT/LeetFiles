7e185390d97396586133f2cd25019172da2d4936
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-12 13:49:18 +0530

    Update 1863 Sum of all subset XOR Totals.java

--- 1863 Sum of all subset XOR Totals.java
+++ 1863 Sum of all subset XOR Totals.java
@@ -3,3 +3,10 @@ class Solution {
         
         return getAns(nums,0,0);
     }
+   
+    int getAns(int[] arr,int i,int cur){
+
+        if(i==arr.length) return cur;
+        return getAns(arr,i+1,cur^arr[i]) + getAns(arr,i+1,cur);
+    }
+}
