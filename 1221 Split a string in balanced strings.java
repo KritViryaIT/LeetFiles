9e9296dfab87fdb6cd6ffb3ed55a08d0122a0333
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-17 13:42:27 +0530

    Update 1221 Split a string in balanced strings.java

--- 1221 Split a string in balanced strings.java
+++ 1221 Split a string in balanced strings.java
@@ -2,3 +2,15 @@ class Solution {
     public int balancedStringSplit(String s) {
         
         int count = 0, countL = 0, countR = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='R')
+                countR++;
+            else
+                countL++;
+            if(countR==countL)
+                count++;
+        }
+        return count;
+    }
+}
