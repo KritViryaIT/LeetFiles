a16947b66eb53651f6aa20f8dac4507c99bb02c1
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-17 13:32:17 +0530

    Update 1678 Goal Parser Interpretation.java

--- 1678 Goal Parser Interpretation.java
+++ 1678 Goal Parser Interpretation.java
@@ -31,3 +31,10 @@ class Solution {
     public String interpret(String command) {
         
         StringBuilder s = new StringBuilder(command.length());
+        for (int i = 0; i < command.length(); i++)
+        {
+            if (command.charAt(i) == 'G')
+                s.append("G");
+            
+            else if (command.charAt(i) == '(' && command.charAt(i + 1) == ')')
+                
