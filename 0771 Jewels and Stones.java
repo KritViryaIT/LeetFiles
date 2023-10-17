b8240da6ba35e964432e8c6bd7440b9065b7755d
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-17 13:07:09 +0530

    Update 0771 Jewels and Stones.java

--- 0771 Jewels and Stones.java
+++ 0771 Jewels and Stones.java
@@ -22,3 +22,7 @@ class Solution {
 
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
+
+        int count = 0;
+        for (int i = 0; i < jewels.length(); i++)
+        {
