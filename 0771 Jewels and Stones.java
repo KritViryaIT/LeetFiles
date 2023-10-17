701da0622782f8de1d9d3d73a2274db48f4daf58
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-17 13:03:00 +0530

    Update and rename 771 Jewels and Stones.java to 0771 Jewels and Stones.java

---
+++ 0771 Jewels and Stones.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        
+        List<String> jlist = new ArrayList<String>(Arrays.asList(jewels.split("")));
+        List<String> slist = new ArrayList<String>(Arrays.asList(stones.split("")));
+
+        int count = 0;
+        for (String j : jlist)
+        {
+            for (String s : slist)
+            {
+                if (j.equals(s))
+                    count++;
+            }
+        }
+
+        return count;
+    }
+}
