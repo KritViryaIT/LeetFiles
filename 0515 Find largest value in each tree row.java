ca5a30b472eb16adf3bf4019c5da0def0e6b1df2
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-24 23:08:11 +0530

    Update and rename 515 Find largest value in each tree row.java to 0515 Find largest value in each tree row.java

---
+++ 0515 Find largest value in each tree row.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public static void calc(TreeNode root,List<Integer> list,int height) {
+
+        if(root==null) return;
+        int curr=root.val;
+        if(list.size()==height) list.add(curr);
+
+        else 
+            if(curr>list.get(height)) 
+                list.set(height,curr);
+            
+        calc(root.right,list,height+1);
+        calc(root.left,list,height+1);
+
+        return;
+
+    }
+    public List<Integer> largestValues(TreeNode root) {
+
+        List<Integer> list  = new ArrayList<>();
+        calc(root,list,0);
+        
+        return list;
+    }
+}
