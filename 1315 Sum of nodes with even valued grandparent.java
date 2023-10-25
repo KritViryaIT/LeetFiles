dde74686b11501c0f1c8c714bc6310f12ac551d9
Kritvirya Singh <111434417+KritViryaIT@users.noreply.github.com>
2023-10-25 15:02:00 +0530

    Update 1315 Sum of nodes with even valued grandparent.java

--- 1315 Sum of nodes with even valued grandparent.java
+++ 1315 Sum of nodes with even valued grandparent.java
@@ -1,1 +1,25 @@
+class Solution {
+    int sum = 0;
+
+    public int sumEvenGrandparent(TreeNode root) {
+
+        sumVal(root);
+        return sum;
+    }
+
+    public void sumVal(TreeNode root) {
+
+        if(root==null) return;
+
+        if(root.val%2==0)
+        {
+            if(root.left!=null && root.left.left!=null) sum += root.left.left.val;
+            if(root.left!=null && root.left.right!=null) sum += root.left.right.val;
+            if(root.right!=null && root.right.left!=null) sum += root.right.left.val;
+            if(root.right!=null && root.right.right!=null) sum += root.right.right.val;
+        }
+        sumVal(root.left);
+        sumVal(root.right);
+    }
+}
 
